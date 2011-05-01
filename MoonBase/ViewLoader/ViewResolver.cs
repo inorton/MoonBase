64f4671b57d1959b6bef4e2b8be18f58902e8c49
Ian Norton <inb@ncipher.com>
2011-05-01 16:58:49 +0100

    more restructuring

---
+++ MoonBase/ViewLoader/ViewResolver.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MoonDesk
+{
+  public class ViewResolver {
+
+    public ViewLoader Loader { get; private set; }
+
+    public ViewResolver ( )
+    {
+      Loader = new ViewLoader( );
+    }
+
+  }
+}
